0e0709a66024f9c48a01aa40c866b5a1b76bc6b3
elkiki99 <brossani23@gmail.com>
2024-11-19 19:34:18 -0300

    m

--- resources/views/livewire/components/toggle-watchlist.blade.php
+++ resources/views/livewire/components/toggle-watchlist.blade.php
@@ -7,20 +7,13 @@ use App\Models\Watchlist;
 new class extends Component {
     public $itemId;
     public $itemType;
-    public $inWatchlist = false;
+    public $inWatchlist = [];
 
     public function mount($itemId, $itemType)
     {
         $this->itemId = $itemId;
         $this->itemType = $itemType;
         $this->inWatchlist = Auth::user()->watchlist->where('item_type', $itemType)->pluck('item_id')->contains($itemId);
-    }
-
-    public function getListeners()
-    {
-        return [
-            'watchlistUpdated' => 'syncWatchlist',
-        ];
     }
 
     public function toggleWatchlist()
@@ -32,15 +25,7 @@ new class extends Component {
         }
 
         $this->inWatchlist = !$this->inWatchlist;
-
-        $this->dispatch('watchlistUpdated', $this->itemId, $this->itemType, $this->inWatchlist);
-    }
-
-    public function syncWatchlist($itemId, $itemType, $inWatchlist)
-    {
-        if ($this->itemId === $itemId && $this->itemType === $itemType) {
-            $this->inWatchlist = $inWatchlist;
-        }
+        $this->dispatch('watchlistUpdated', $this->inWatchlist, $this->itemId, $this->itemType);
     }
 
     private function addToWatchlist()
@@ -62,53 +47,27 @@ new class extends Component {
 }; ?>
 
 <div>
-    <div class="hidden sm:block">
-        <button class="block w-full text-left" wire:click="toggleWatchlist">
-            @if ($inWatchlist)
-                <div class="flex items-center gap-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="size-4 sm:size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="m3 3 1.664 1.664M21 21l-1.5-1.5m-5.485-1.242L12 17.25 4.5 21V8.742m.164-4.078a2.15 2.15 0 0 1 1.743-1.342 48.507 48.507 0 0 1 11.186 0c1.1.128 1.907 1.077 1.907 2.185V19.5M4.664 4.664 19.5 19.5" />
-                    </svg>
+    <button class="block w-full text-left" wire:click="toggleWatchlist">
+        @if ($inWatchlist)
+            <div class="flex items-center gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="size-4 sm:size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="m3 3 1.664 1.664M21 21l-1.5-1.5m-5.485-1.242L12 17.25 4.5 21V8.742m.164-4.078a2.15 2.15 0 0 1 1.743-1.342 48.507 48.507 0 0 1 11.186 0c1.1.128 1.907 1.077 1.907 2.185V19.5M4.664 4.664 19.5 19.5" />
+                </svg>
 
-                    <p class="text-xs sm:text-sm">Remove from watchlist</p>
-                </div>
-            @else
-                <div class="flex items-center gap-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="size-4 sm:size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
-                    </svg>
+                <p class="text-xs sm:text-sm">Remove from watchlist</p>
+            </div>
+        @else
+            <div class="flex items-center gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="size-4 sm:size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
+                </svg>
 
-                    <p class="text-xs sm:text-sm">Add to watchlist</p>
-                </div>
-            @endif
-        </button>
-    </div>
-
-    <div class="absolute z-30 sm:hidden top-2 right-2">
-        <button class="block w-full text-left" wire:click="toggleWatchlist">
-            @if ($inWatchlist)
-                <div class="flex items-center gap-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                        class="text-white size-4">
-                        <path fill-rule="evenodd"
-                            d="M6.32 2.577a49.255 49.255 0 0 1 11.36 0c1.497.174 2.57 1.46 2.57 2.93V21a.75.75 0 0 1-1.085.67L12 18.089l-7.165 3.583A.75.75 0 0 1 3.75 21V5.507c0-1.47 1.073-2.756 2.57-2.93Z"
-                            clip-rule="evenodd" />
-                    </svg>
-                </div>
-            @else
-                <div class="flex items-center gap-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="text-white size-4">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
-                    </svg>
-                </div>
-            @endif
-        </button>
-    </div>
+                <p class="text-xs sm:text-sm">Add to watchlist</p>
+            </div>
+        @endif
+    </button>
 </div>
-
